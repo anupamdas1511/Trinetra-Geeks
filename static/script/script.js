4f1db5bf98b87c3e3682d19a5d807a962135ef77
Anupam <escapevel11.2@gmail.com>
2022-02-26 18:08:46 +0530

    updates

--- static/script/script.js
+++ static/script/script.js
@@ -12,15 +12,8 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-<<<<<<< HEAD
 });
 function nextClick(){
   document.getElementById("next").click();
 }
-
 setInterval(nextClick, 1500);
-=======
-  });
-
-  
->>>>>>> 143fcfa6753c637ce9d14c9c144d123bce8b13f0
