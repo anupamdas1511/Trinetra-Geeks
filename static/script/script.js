3a375cc2b7cdbb0cd14aad72a5b958e2d9f7ab4b
Anupam <escapevel11.2@gmail.com>
2022-02-26 13:59:21 +0530

    Added swiper.js

---
+++ static/script/script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    // If you want to use pagination then uncomment below
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   clickable: true,
+    // },
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  });
